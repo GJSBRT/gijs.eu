f7c1add5efdc97fbe900701d22812ae95eb105b4
Gijsbert <gijsbert@dutchis.net>
2023-11-25 18:38:13 +0100

    Added background tiles and fixed favicon

--- src/elements/Container.tsx
+++ src/elements/Container.tsx
@@ -1,11 +1,24 @@
 import { Outlet } from "react-router-dom";
 
+// @ts-expect-error - SVG file
+import BackgroundTile from '../assets/svgs/background-tile.svg';
+
 export default function() {
+    const style = {
+        backgroundImage: `url(${BackgroundTile})`,
+        backgroundSize: '750px',
+        opacity: '0.1',
+    }
+
     return (
-        <main className="dark:bg-gray-950 bg-gray-50 min-h-screen dark:text-white text-black">
-            <div className="max-w-3xl mx-auto py-8 flex flex-col">
-                <Outlet/>
+        <main className="min-h-screen relative bg-opacity-50 dark:text-white text-black dark:bg-gray-950 bg-gray-50">
+            <div className="absolute w-full h-full z-10">
+                <div className="max-w-3xl mx-auto py-8 flex flex-col">
+                    <Outlet/>
+                </div>
             </div>
+
+            <div style={style} className="bg-center bg-repeat min-h-screen absolute h-full w-full z-0"></div>
         </main>
     )
 }
