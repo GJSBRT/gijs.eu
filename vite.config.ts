2f112b6047b26ceab5ed81a9b84577bcc4d33769
Gijsbert <gijsbert@dutchis.net>
2023-12-08 18:49:09 +0100

    Added million.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import million from 'million/compiler';
 import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     plugins: [
+        million.vite({ 
+            auto: true 
+        }),
         react(),
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
